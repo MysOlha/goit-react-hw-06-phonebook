ac18f170a20df8424f68a462d155268a7bdd92b1
MysOlha <misarosh70@gmail.com>
2022-11-20 20:37:27 +0200

    add pattern

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -65,8 +65,8 @@ export const Form = () => {
         onChange={handleChangeInput}
         id={nameFormId}
         className={css.inputForm}
-        // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
       />
 
@@ -79,8 +79,8 @@ export const Form = () => {
         onChange={handleChangeInput}
         id={numberFormId}
         className={css.inputForm}
-        // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
       <button type="submit" className={css.btnForm}>
